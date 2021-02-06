6337d1f2d74804a60a2887e815e60a744564a5e5
Andrey Ryzhkov <rizkoff@gmail.com>
2021-02-06 17:48:46 +0300

    S3 state remote backend

---
+++ terraform/stage/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = var.s3_state_backend_bucket
+    region     = "ru-central1"
+    key        = "stage/terraform.tfstate"
+    access_key = var.s3_access_key
+    secret_key = var.s3_secret_key
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
